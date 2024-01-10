2ea09285406a913e9450af852911fdbc58448b68
Innocent-Traitor <jamesmoffett101@gmail.com>
2024-01-10 15:37:15 -0600

    redux add

--- src/Editor.js
+++ src/Editor.js
@@ -1,16 +1,38 @@
 import './App.css';
+import React from 'react';
 
-function Editor() {
-  return (
-    <div className="editor">
-      <header className='header'>
-        <p className='header-text' for='editor'>Markdown Editor</p>
-      </header>
-      <textarea className='editor-area' id='editor' placeholder='text'>
+class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+    }
 
-      </textarea>
-    </div>
-  );
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event){
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="editor">
+        <header className='header'>
+          <p className='header-text' for='editor'>Markdown Editor</p>
+        </header>
+        <textarea 
+          className='editor-area' 
+          id='editor' 
+          placeholder='text' 
+          value={this.state.input} 
+          onChange={this.handleInput}>
+        </textarea>
+      </div>
+    );
+  }
 }
 
 export default Editor;
--- src/Preview.js
+++ src/Preview.js
@@ -1,16 +1,26 @@
 import './App.css';
+import React from 'react';
 
-function Preview() {
-    return (
-      <div className="preview-div">
-        <header className='preview-header'>
-            <p className='header-text'>Markdown Preview</p>
-        </header>
-        <div className='preview'>
+class Preview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: ''
+        }
+    }
 
-        </div>
-      </div>
-    );
-  }
-  
-  export default Preview;
+    render() {
+        return (
+            <div className="preview-div">
+              <header className='preview-header'>
+                  <p className='header-text'>Markdown Preview</p>
+              </header>
+              <div className='preview'>
+      
+              </div>
+            </div>
+          );
+    }
+}
+
+  export default Preview; 
