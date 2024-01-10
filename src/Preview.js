21ed1690270190332fc02d28b6d6396241cca278
Innocent-Traitor <jamesmoffett101@gmail.com>
2024-01-10 16:41:22 -0600

    Maybe working, need to config CSS

--- src/Preview.js
+++ src/Preview.js
@@ -1,13 +1,15 @@
 import './App.css';
+import { Marked, marked } from 'marked';
 
 function Preview({value}) {
+    let element = marked.parse(value);
+    console.log(element);
     return (
       <div className="preview-div">
         <header className='preview-header'>
             <p className='header-text'>Markdown Preview</p>
         </header>
-        <div className='preview'>
-            {value}
+        <div className='preview' dangerouslySetInnerHTML={{__html: element}}>
         </div>
       </div>
     );
